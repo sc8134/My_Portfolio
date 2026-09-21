752f9ea2af671ac150478b5efc286a464f6c9b42
Sagar RC <sc81341@gmail.com>
2026-09-22 02:16:51 +0545

    fix: rename background/ to img/ (InfinityFree blocks that dir), fix hero text overflow on mobile

--- src/features/about/About.tsx
+++ src/features/about/About.tsx
@@ -34,7 +34,7 @@ export function About() {
               }}
             >
               <img
-                src="/background/professional.webp"
+                src="/img/professional.webp"
                 alt="Sagar Roka Chhetri"
                 style={{ width: '100%', height: '100%', objectFit: 'cover' }}
                 onError={(e) => {
